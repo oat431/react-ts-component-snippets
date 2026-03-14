64ff0d25fda4aaf27d3a202db4b80b0f6ba5c96b
oat431 <oat431@gmail.com>
2026-03-14 22:41:25 +0700

    add: login page for login feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
+import LoginPage from './pages/LoginPage';
 
 const ReactRouter = createBrowserRouter([
     { path: '/', Component: HomePage },
-    { path: '/contact', Component: ContactPage }
+    { path: '/contact', Component: ContactPage },
+    { path: '/login', Component: LoginPage }
 ]);
 
 export default ReactRouter
